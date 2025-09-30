81a28717203286372033bf5ea410cef3eaca9cd8
murad-1999 <slackenheim@gmail.com>
2025-09-30 22:02:40 +0300

    reply stored as dataframe

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,6 +1,6 @@
 import requests
-import praw
 import pandas as pd
+import json
 from creds import client_id, client_secret, username, password, user_agent
 
 
@@ -10,17 +10,19 @@ data = {"grant_type": "password", "username": username, "password": password}
 headers = {"User-Agent": user_agent}
 
 res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
-print(res.json())
 
-""" token = res.json()["access_token"]
+token = res.json()["access_token"]
 headers = {**headers, **{"Authorization": f"bearer {token}"}}
 
 res = requests.get("https://oauth.reddit.com/r/astronomy/top",
-                   headers=headers, params={"limit": 500})
-for post in res.json()["data"]["children"]:
-    print(post["data"]["title"])
+                   headers=headers, params={"limit": 5})
+
+df = pd.DataFrame(res.json()["data"]["children"])
+print(df.head())    
 
 
- """
+""" 
 
-
+with open("data.json", "w") as json_file:
+        json.dump(res.json, json_file, indent=4)
+        print("Data appended to data.json file.") """
