6810f4e879313889f5f702dff0a71a12e3e9395c
murad-1999 <slackenheim@gmail.com>
2025-09-30 22:42:24 +0300

    Serialized and encoded JSON dict to a string in prod. added function call in main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from producer import run_producer
 from consumer import run_consumer
 import threading
 import time
+from fetch_data import fetch_reddit_data
 
 def main():
     # Start consumer in background thread
@@ -10,8 +11,10 @@ def main():
 
     time.sleep(2)  # give consumer a moment to connect
 
+    data = fetch_reddit_data('astronomy', limit=1)
+
     # Send one message
-    run_producer("test_topic", "Hello")
+    run_producer("test_topic", data)
 
     # Keep alive so consumer can print
     time.sleep(5)
