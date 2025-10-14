7f7d40b4e47774610a06b7f11ed7d33a24407d4f
murad-1999 <slackenheim@gmail.com>
2025-10-14 10:18:34 +0300

    replaced env vars

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,8 +1,18 @@
 import requests
 import pandas as pd
 import json
-from creds import client_id, client_secret, username, password, user_agent
+import os
+#from creds import client_id, client_secret, username, password, user_agent
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("REDDIT_CLIENT_ID")
+client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+username = os.getenv("REDDIT_USERNAME")
+password = os.getenv("REDDIT_PASSWORD")
+user_agent = os.getenv("REDDIT_USER_AGENT")
 
 def fetch_reddit_data(subreddit="astronomy", limit=5):
     """Fetch top posts from a specified subreddit."""
@@ -17,8 +27,7 @@ def fetch_reddit_data(subreddit="astronomy", limit=5):
 
     res = requests.get(f"https://oauth.reddit.com/r/{subreddit}/top",
                     headers=headers, params={"limit": limit, "t": "day"})
-
-    # df = pd.DataFrame(res.json()["data"]["children"])
+    # res.raise_for_status() 
     return res.json()
 
 
