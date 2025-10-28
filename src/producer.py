b2553e9e9b3a7fa8fa218d4814e5fc38f2205d4d
murad-1999 <slackenheim@gmail.com>
2025-10-28 19:13:15 +0300

    set producer to be idempotent and retries to 5

--- src/producer.py
+++ src/producer.py
@@ -2,7 +2,8 @@ from kafka import KafkaProducer
 import json
 def run_producer(topic: str, message: bytes):
     """Send a message to a Kafka topic."""
-    producer = KafkaProducer(bootstrap_servers="localhost:9092")
+    producer = KafkaProducer(bootstrap_servers="localhost:9092",
+    retries=5)
     producer.send(topic, json.dumps(message).encode('utf-8'))
     producer.flush()
     print(f"Sent:msg")
